97413ab0617028c1f289fc8ee06986291d318023
sorleone <sorleone@protonmail.ch>
2019-01-28 21:05:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1>DoXTract</h1>
+
+Very Fast Pure JS Text Extractor For Your Office Files.
+
+<h2>Installation</h2>
+
+```
+npm i doxtract
+```
+
+<h2>Example</h2>
+
+```js
+const { extractText } = require('doxtract');
+extractText('./file.docx').then(console.log).catch(console.error);
+```
+
+<h2>Contribution</h2>
+<p>
+  Please report issues/bugs, feature requests and suggestions for improvements to the <a href="https://github.com/sorleone/doxtract/issues">issue tracker</a>.
+</p>
+
+<p align="center">
+  <a href="https://creativecommons.org/licenses/by-nc-sa/4.0">
+    <img src="http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-nc-sa.svg"/>
+  </a>
+  <br>
+  Copyright &copy; 2019
+</p>
---
+++ doxtract.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const AdmZip = require('adm-zip');
+const { extname } = require('path');
+
+const extensionRegex = new RegExp('^(.docx|.xlsx|.pptx)$');
+const paragraphRegex = new RegExp('(<w:t>|<w:t xml:space="preserve">)[^]*?(?=<\/w:p>)', 'g');
+const textBlockRegex = new RegExp('(<w:t>|<w:t xml:space="preserve">)([^<]*)', 'g');
+
+function getXMLData(path, xmlFilename) {
+  return new Promise((resolve, reject) => {
+    if (!extensionRegex.test(extname(path))) {
+      reject(new Error('The file must be either a .docx, .xlsx or .pptx'));
+    }
+    try {
+      const zip = new AdmZip(path);
+      zip.getEntry(`word/${xmlFilename}.xml`).getDataAsync(
+        data => resolve(data.toString())
+      );
+    } catch(err) {
+      reject(`${err} (${path})`);
+    }
+  });
+}
+
+/**
+ * Extracts the text from your Office file.
+ * 
+ * @param {String} path Path to the file you want to extract the text from.
+ * @param {String} [xmlFilename='document'] Optional argument used to specify
+ * the XML component of the file from which to extract the text (default is: 'document').
+ */
+module.exports.extractText = (path, xmlFilename = 'document') => {
+  return getXMLData(path, xmlFilename).then((xml) => {
+    let paragraph, textBlock, text = '';
+    while (paragraph = paragraphRegex.exec(xml)) {
+      while (textBlock = textBlockRegex.exec(paragraph[0])) {
+        text += textBlock[2];
+      }
+      text += '\n\n';
+    }
+    return text;
+  });
+}
